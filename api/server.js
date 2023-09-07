5d070f47636f26de39fad5281251ede814d39bf0
JWLK <utxtion@me.com>
2023-09-07 21:44:32 +0900

    ADD CONFIG

--- api/server.js
+++ api/server.js
@@ -1,7 +1,13 @@
+require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` })
+
+const PORT = process.env.PORT || 3000
+const CORS_ORIGIN = process.env.CORS_ORIGIN || '*'
+
 const express = require('express')
 const cors = require('cors')
 const http = require('http')
 const socketIo = require('socket.io')
+const crypto = require('crypto')
 
 const app = express()
 app.set('trust proxy', true) // 프록시 뒤에서 실행되는 경우
@@ -10,33 +16,48 @@ app.use(cors())
 const server = http.createServer(app)
 const io = require('socket.io')(server, {
     cors: {
-        origin: 'http://localhost:3001',
+        origin: CORS_ORIGIN,
         methods: ['GET', 'POST'],
-        credentials: true,
+        credentials: false,
     },
 })
+
+const usersLocation = new Map()
 
 app.get('/', (req, res) => {
     res.send('WebSocket Server Running!')
 })
 
 io.on('connection', (socket) => {
-    console.log('a user connected')
+    const hash = crypto.randomBytes(8).toString('hex')
+    socket.emit('client-hash', hash) // 연결된 클라이언트에게 자신의 hash 값을 전송
 
-    let ips =
-        socket.handshake.headers['x-forwarded-for'] || socket.conn.remoteAddress
-    const clientIp = ips.split(',')[0]
+    console.log('a user connected :: ' + hash)
 
-    socket.on('share location', (location) => {
-        // console.log(`${clientIp} - [${location.lat}, ${location.lng}]`)
-        io.emit('update location', { ...location, ip: clientIp })
+    let clientIp =
+        socket.request.headers['x-forwarded-for'] ||
+        socket.request.connection.remoteAddress
+    if (clientIp.substr(0, 7) == '::ffff:') {
+        clientIp = clientIp.substr(7)
+    }
+
+    socket.on('share-location', (location) => {
+        const userData = {
+            ...location,
+            ip: clientIp,
+            hash,
+        }
+        usersLocation.set(hash, userData)
+
+        io.emit('update-location', Array.from(usersLocation.values()))
     })
 
     socket.on('disconnect', () => {
         console.log('user disconnected')
+        usersLocation.delete(hash)
     })
 })
 
-server.listen(3000, () => {
-    console.log('listening on *:3000')
+server.listen(PORT, () => {
+    console.log(`listening on *:${PORT}`)
 })
--- front/components/GoogleMapsWrapper.js
+++ front/components/GoogleMapsWrapper.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useRef } from 'react'
+
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'
 // import { InfoWindow } from '@react-google-maps/api'; // 현재 InfoWindow는 주석 처리 되어 있음
 
@@ -17,9 +18,13 @@ const initialCenter = {
 }
 
 export function GoogleMapsWrapper({ children, isSharingEnabled, isCentered }) {
-    const [markerData, setMarkerData] = useState(null)
+    const [markerData, setMarkerData] = useState([])
     const [mapCenter, setMapCenter] = useState(initialCenter)
     const hasCenterBeenSetRef = useRef(false)
+    const ownHashCode = useRef(null)
+
+    const SOCKET_SERVER_URL =
+        process.env.NEXT_PUBLIC_SOCKET_SERVER_URL || 'http://localhost:3000'
 
     useEffect(() => {
         if (!isSharingEnabled) {
@@ -27,7 +32,7 @@ export function GoogleMapsWrapper({ children, isSharingEnabled, isCentered }) {
             return // 위치 공유가 비활성화면 아무 것도 하지 않음
         }
 
-        const socket = io('http://localhost:3000')
+        const socket = io(SOCKET_SERVER_URL)
 
         socket.on('connect', () => {
             console.log('Connected to the server')
@@ -37,16 +42,35 @@ export function GoogleMapsWrapper({ children, isSharingEnabled, isCentered }) {
             console.error('Connection error:', error)
         })
 
-        socket.on('update location', (data) => {
-            setMarkerData(data)
-            if (!hasCenterBeenSetRef.current) {
-                // 추가: 최초 설정이 아직 안 되었을 때만 실행
-                setMapCenter(data)
-                hasCenterBeenSetRef.current = true
-            } else if (isCentered) {
-                setMapCenter(data)
-            }
+        socket.on('client-hash', (hash) => {
+            console.log('hash', hash)
+            ownHashCode.current = hash
         })
+
+        const handleLocationUpdate = (dataList) => {
+            // console.log('dataList', dataList)
+            const updatedDataList = dataList.map((data) => {
+                if (data.hash === ownHashCode.current) {
+                    data.isMe = true
+                }
+                return data
+            })
+
+            // dataList에서만 존재하는 사용자를 markerData 상태에 반영
+            setMarkerData(updatedDataList)
+
+            // 자신의 위치 데이터에 대한 처리
+            const ownData = updatedDataList.find((data) => data.isMe)
+            if (ownData) {
+                if (!hasCenterBeenSetRef.current) {
+                    setMapCenter(ownData)
+                    hasCenterBeenSetRef.current = true
+                } else if (isCentered) {
+                    setMapCenter(ownData)
+                }
+            }
+        }
+        socket.on('update-location', handleLocationUpdate)
 
         const updateLocation = () => {
             if (navigator.geolocation) {
@@ -56,21 +80,24 @@ export function GoogleMapsWrapper({ children, isSharingEnabled, isCentered }) {
                         lat: latitude,
                         lng: longitude,
                     }
-                    socket.emit('share location', location)
+                    // console.log('newData :: ' + JSON.stringify(location))
+                    socket.emit('share-location', location)
                 })
             }
         }
-
-        const intervalId = setInterval(updateLocation, 300)
+        const intervalId = setInterval(updateLocation, 1000)
 
         return () => {
+            socket.off('update location', handleLocationUpdate)
             socket.disconnect()
             clearInterval(intervalId)
         }
-    }, [isCentered, isSharingEnabled])
+    }, [isCentered, isSharingEnabled, SOCKET_SERVER_URL])
 
     return (
-        <LoadScript googleMapsApiKey="AIzaSyBF5q06rev3BDsaD9vajgYaaCM5UpQvlUU">
+        <LoadScript
+            googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}
+        >
             {children}
             <GoogleMap
                 mapContainerStyle={containerStyle}
@@ -85,16 +112,34 @@ export function GoogleMapsWrapper({ children, isSharingEnabled, isCentered }) {
                     zoomControl: true,
                 }}
             >
-                {markerData && (
-                    <Marker
-                        position={markerData}
-                        label={{
-                            text: markerData.ip,
-                            color: 'black',
-                            fontSize: '12px',
-                        }}
-                    />
-                )}
+                {markerData &&
+                    markerData.map((data, index) => {
+                        // console.log(`Data ${index} ::  ${JSON.stringify(data)}`)
+                        // const isMe = data.hash === myHash
+
+                        return (
+                            <Marker
+                                key={data.hash}
+                                position={{ lat: data.lat, lng: data.lng }}
+                                label={{
+                                    text: data.isMe ? 'You' : 'other',
+                                    color: 'white',
+                                    fontSize: '10px',
+                                }}
+                                icon={{
+                                    path: google.maps.SymbolPath.CIRCLE,
+                                    scale: 15, // 마커의 크기
+                                    fillColor: data.isMe ? 'red' : 'blue', // 자신은 빨간색, 다른 사용자는 파란색
+                                    fillOpacity: 0.7,
+                                    strokeColor: 'white',
+                                    strokeOpacity: 1,
+                                    strokeWeight: 1,
+                                    // url: 'path_to_your_image.png',
+                                    // scaledSize: new google.maps.Size(40, 40), // 아이콘 이미지 크기를 40x40 픽셀로
+                                }}
+                            />
+                        )
+                    })}
             </GoogleMap>
         </LoadScript>
     )
