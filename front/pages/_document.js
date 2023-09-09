494cae01bf0dc14eae3ce3698f96ce6318e1d03e
JWLK <utxtion@me.com>
2023-09-09 23:59:30 +0900

    add config hedaer

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -4,12 +4,17 @@ export default function Document() {
     return (
         <Html lang="kr">
             <Head>
-                {/* <meta name="viewport" content="viewport-fit=cover" /> */}
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scale=1.0, user-scalable=no, viewport-fit=cover, shrink-to-fit=no"
+                ></meta>
                 <link rel="manifest" href="/manifest.json" />
+                <meta name="apple-mobile-web-app-capable" content="yes"></meta>
                 <meta
                     name="apple-mobile-web-app-status-bar-style"
                     content="black-translucent"
                 />
+                <meta name="mobile-web-app-capable" content="yes"></meta>
                 <meta name="theme-color" content="#373737" />
             </Head>
             <body>
