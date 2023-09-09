7409fef2a578f75a89b211b3e98c12f50064c927
JWLK <utxtion@me.com>
2023-09-09 23:00:22 +0900

    add theme color

--- front/pages/_document.js
+++ front/pages/_document.js
@@ -2,9 +2,16 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
     return (
-        <Html lang="en">
-            <meta name="viewport" content="viewport-fit=cover" />
-            <Head />
+        <Html lang="kr">
+            <Head>
+                {/* <meta name="viewport" content="viewport-fit=cover" /> */}
+                <meta
+                    name="apple-mobile-web-app-status-bar-style"
+                    content="black-translucent"
+                />
+                <meta name="theme-color" content="#373737" />
+                {/* 원하는 색상으로 변경 */}
+            </Head>
             <body>
                 <Main />
                 <NextScript />
