3f3460a9a22adee25fa29fa3e1d5e0a8ea547403
JWLK <utxtion@me.com>
2023-09-10 00:14:53 +0900

    add

--- front/pages/index.js
+++ front/pages/index.js
@@ -30,7 +30,7 @@ export default function Home() {
                 <div
                     style={{
                         position: 'absolute',
-                        top: 'calc(constant(safe-area-inset-bottom) + 20px)',
+                        top: 'calc(constant(safe-area-inset-top) + 20px)',
                         left: 3,
                         zIndex: 10,
                         padding: '10px',
