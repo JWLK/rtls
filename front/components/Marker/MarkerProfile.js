777f209d2054f35b0d53090defc769dfd0a33472
JWLK <utxtion@me.com>
2023-09-18 00:32:57 +0900

    Edit flow touch action

--- front/components/Marker/MarkerProfile.js
+++ front/components/Marker/MarkerProfile.js
@@ -7,8 +7,8 @@ const MarkerProfile = (props) => {
     const { width, height } = props
     return `data:image/svg+xml;utf-8, 
     <svg width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+        <image x="30" y="30" width="40" height="40" xlink:href="./IMG_1709.png" alt="My Happy SVG" />
         <circle cx="50" cy="50" r="40" stroke="white" stroke-width="5" fill="green" />
-        <img src="./IMG_1709.png" alt="My Happy SVG" />
     </svg>`
 }
 
